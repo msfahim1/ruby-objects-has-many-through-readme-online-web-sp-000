8f2136ce717310f7267924225ce1b858a3b5f9b0
Michael Fahim <michael_fahim@yahoo.com>
2020-01-25 18:38:01 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,35 @@
 class Customer
-  
-end
+  attr_accessor :name, :age
+
+  @@all = []
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(waiter, total, tip)
+    Meal.new(waiter, self, total, tip)
+  end
+
+  def meals
+    customer_meals = []
+    Meal.all.each do |meal|
+      if meal.customer.name == @name
+        customer_meals << meal
+      end
+    end
+    customer_meals
+  end
+
+  def waiters
+    customer_waiters = []
+    meals.each{|meal| customer_waiters << meal.waiter}
+    customer_waiters
+  end
+end 
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,45 @@
 class Waiter
 
-end
+  attr_accessor :name, :years_of_experience
+
+  @@all = []
+
+  def initialize(name, years)
+    @name = name
+    @years_of_experience = years
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(customer, total, tip)
+    Meal.new(self, customer, total, tip)
+  end
+
+  def meals
+    waiter_meals = []
+    Meal.all.each do |meal|
+      if meal.waiter.name == @name
+        waiter_meals << meal
+      end
+    end
+    waiter_meals
+  end
+
+  def best_tipper
+    biggest_tip = nil
+    biggest_tipper = nil
+    meals.each do |meal|
+      if biggest_tipper == nil
+        biggest_tip = meal.tip
+        biggest_tipper = meal.customer
+      elsif meal.tip > biggest_tip
+        biggest_tip = meal.tip
+        biggest_tipper = meal.customer
+      end
+    end
+    biggest_tipper
+  end
+end 
