3c499e234793c49353c0d34cc7fb902360ac1e63
Michael Fahim <michael_fahim@yahoo.com>
2020-01-25 18:37:19 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,35 +1,35 @@
 class Customer
-  attr_accessor :name, :age 
+  attr_accessor :name, :age
 
   @@all = []
 
   def initialize(name, age)
-    @name = name 
+    @name = name
     @age = age
-    @@all << self 
+    @@all << self
   end
 
-  def self.all 
+  def self.all
     @@all
-  end 
+  end
 
   def new_meal(waiter, total, tip)
-    Meal.new(waiter, self, total, tip)  
-  end 
+    Meal.new(waiter, self, total, tip)
+  end
 
   def meals
     customer_meals = []
     Meal.all.each do |meal|
-      if meal.customer.name == @name 
+      if meal.customer.name == @name
         customer_meals << meal
-      end 
+      end
     end
     customer_meals
-  end 
+  end
 
-  def waiters 
+  def waiters
     customer_waiters = []
     meals.each{|meal| customer_waiters << meal.waiter}
     customer_waiters
-  end 
+  end
 end 
