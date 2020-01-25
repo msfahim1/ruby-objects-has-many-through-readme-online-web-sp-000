72f8f49391cd3236b41865c98609eedbb21ac990
Michael Fahim <michael_fahim@yahoo.com>
2020-01-25 18:37:47 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,12 +1,12 @@
 class Waiter
 
-  attr_accessor :name, :yrs_experience
+  attr_accessor :name, :years_of_experience
 
   @@all = []
 
-  def initialize(name, yrs_experience)
+  def initialize(name, years)
     @name = name
-    @yrs_experience = yrs_experience
+    @years_of_experience = years
     @@all << self
   end
 
@@ -14,4 +14,32 @@ class Waiter
     @@all
   end
 
-end
+  def new_meal(customer, total, tip)
+    Meal.new(self, customer, total, tip)
+  end
+
+  def meals
+    waiter_meals = []
+    Meal.all.each do |meal|
+      if meal.waiter.name == @name
+        waiter_meals << meal
+      end
+    end
+    waiter_meals
+  end
+
+  def best_tipper
+    biggest_tip = nil
+    biggest_tipper = nil
+    meals.each do |meal|
+      if biggest_tipper == nil
+        biggest_tip = meal.tip
+        biggest_tipper = meal.customer
+      elsif meal.tip > biggest_tip
+        biggest_tip = meal.tip
+        biggest_tipper = meal.customer
+      end
+    end
+    biggest_tipper
+  end
+end 
